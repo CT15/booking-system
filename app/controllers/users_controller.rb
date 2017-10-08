c177eeb98c547354daeda064b24b8e55200b1bc7
Calvin Tantio <calvin.tantio@yahoo.com>
2017-10-08 18:09:45 +0800

    added users/new

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
 
-        if user.save
+        if @user.save
             redirect_to users_path
         else
             render :new
@@ -23,5 +23,9 @@ class UsersController < ApplicationController
 
     def show
         @user = User.find(params[:id])
+    end
+
+    private def user_params
+        params.require(:user).permit(:full_name, :email, :password)
     end
 end
